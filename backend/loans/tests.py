39e3768c6b21af8a9e1dd90f17c29459a686026a
Princess Poudel <princesspoudel@Princesss-MacBook-Air.local>
2026-04-21 01:32:50 +0545

    Unit test for Loan Eligibility

--- backend/loans/tests.py
+++ backend/loans/tests.py
@@ -2,6 +2,8 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
+from properties.models import Property
+from .models import Loan
 
 User = get_user_model()
 
@@ -41,3 +43,36 @@ class LoanViewSetTests(APITestCase):
         self.assertIn("MonthlyEMI", response.data)
         self.assertIn("TotalPayable", response.data)
         self.assertIn("TotalInterest", response.data)
+
+            # Test loan eligibility check with valid data
+    def test_loan_eligibility_check(self):
+        # Create authenticated user
+        user = User.objects.create_user(
+            username="buyereligibility@example.com",
+            email="buyereligibility@example.com",
+            password="StrongPass123",
+            full_name="Buyer Eligibility",
+            role="buyer",
+            is_verified=True,
+        )
+
+        # Authenticate user
+        self.client.force_authenticate(user=user)
+
+        eligibility_url = reverse("loan-check-eligibility")
+
+        payload = {
+            "income": "1200000.00",
+            "loan_amount": "1000000.00",
+            "existing_emis": "5000.00"
+        }
+
+        # Send POST request
+        response = self.client.post(eligibility_url, payload, format="json")
+
+        # Check response
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("IsEligible", response.data)
+        self.assertIn("MaxAllowedEMI", response.data)
+        self.assertIn("EstimatedEMI", response.data)
+        self.assertIn("Recommendation", response.data)
